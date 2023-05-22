6ec3bea268607fc1738a073cf735c363d90d7c67
betua blaise bashonga <126053707+Blaise-vsc@users.noreply.github.com>
2023-05-22 20:36:15 +0300

    .

--- add.js
+++ add.js
@@ -4,29 +4,34 @@ const image2 = document.querySelector(".main2");
 const span = document.querySelector("span");
 const coffeName = document.querySelector(".coffeName");
 const coffeeBean = document.querySelector(".coffeeBean");
-console.log(span)
+const text = document.querySelector("#navText");
+const text2 = document.querySelector(".textSec");
+
+console.log(text);
 image.addEventListener("click",()=>{
 image.style.display=("none");
 image2.style.display=("flex");
 span.style.color=("yellow");
-coffeeBean.style.display=("none")
+coffeeBean.style.display=("none");
+
+
+
 });
 image2.addEventListener("click",()=>{
 image2.style.display=("none");
 image.style.display=("flex");
 span.style.color=("white");
 });
+
 setTimeout(()=>{
   image2.style.display=("none");  
   image.style.display=("flex"); 
   span.style.color=("yellow");
-},7000);
-setTimeout(()=>{
-  image.style.display=("none");  
-  image2.style.display=("flex");
-  span.style.color=("yellow");  
-},7000);
+  text.style.display=("none");
+  text2.style.display=("block")
+},9000);
+
 setTimeout(()=>{
   image2.style.display=("flex");  
   image.style.display=("none");  
-},7000);
+},9000);
